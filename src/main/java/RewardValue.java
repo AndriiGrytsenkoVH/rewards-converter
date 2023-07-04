8a5b548cfd19e24feaee22b454f79dfe01f76a11
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-07-04 15:01:09 -0500

    tests for cash to miles and back conversion

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,36 @@
-public class RewardValue {
-    double cashValue;
-    int milesValue;
-    double convRate = 0.0035;
-    public RewardValue(double cash){
-        cashValue = cash;
-        milesValue = (int)(cash / convRate);
-    };
+import org.junit.jupiter.api.Test;
 
-    public RewardValue(int miles){
-        cashValue = miles * convRate;
-        milesValue = miles;
-    };
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public double getCashValue(){
-        return cashValue;
+public class RewardValueTests {
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 
-    public int getMilesValue(){
-        return milesValue;
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
+    @Test
+    void convert_from_cash_to_miles() {
+        int milesValue = 10000;
+        double cashValue = 35;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
 
-
+    @Test
+    void convert_from_miles_to_cash() {
+        int milesValue = 10000;
+        double cashValue = 35;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
 }
